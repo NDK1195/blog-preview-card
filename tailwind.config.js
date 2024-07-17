7cfb83252e77d2e0ec70db12866acf13589565d2
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-07-17 20:38:07 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
         grey: 'hsl(0, 0%, 50%)',
         'custom-black': 'hsl(0, 0%, 7%)',
       },
+      boxShadow: {
+        'custom-shadow': '8px 8px 0 0 #000',
+        'custom-shadow-hover': '16px 16px 0 0 #000',
+      },
     },
   },
   plugins: [],
